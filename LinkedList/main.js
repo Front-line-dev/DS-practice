f6fa4932e5d783356823d23df876c5276464aae9
Front-line-dev <kimminsung@kakao.com>
2021-02-27 22:56:07 +0900

    feat: frame module

---
+++ LinkedList/main.js
@@ -0,0 +1,8 @@
+import {drawFrame, addInputListener, print} from '../frame.js'
+
+drawFrame({
+    title: 'Linked List',
+    description: 'Linked List using JavaScript'
+})
+
+addInputListener(print)
--- frame.js
+++ frame.js
@@ -1,1 +1,40 @@
-console.log('test')
+const generateFrame = ({title, description}) => `
+<div class="frame">
+    <header>
+        <h1>${title}</h1>
+        <p>${description}</p>
+    </header>
+    <main>
+        <textarea readonly></textarea>
+        <form id="input-form">
+            <input type="text" id="textInput">
+            <button type="submit" id="textButton">Enter</button>
+        </form>
+    </main>
+    <footer>
+        <a href="../">Return to index page</a>
+    </footer>
+</div>
+`
+
+// Use with onload event
+const drawFrame = ({title, description}) => {
+    document.body.insertAdjacentHTML('afterbegin', generateFrame({title, description}))
+}
+
+const addInputListener = (callback) => {
+    const form = document.querySelector('#input-form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        callback(form.textInput.value)
+        form.textInput.value = ''
+    })
+}
+
+const print = (text) => {
+    const textarea = document.querySelector('textarea')
+    textarea.value += `${text}\n`
+    textarea.scrollTop = textarea.scrollHeight
+}
+
+export {drawFrame, addInputListener, print}
